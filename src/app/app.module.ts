10418e428bfdb0f4a32aced82371c6ad9777108a
abhishek-99ks <abhishek.99ks@gmail.com>
2020-09-07 06:25:30 +0530

    completed the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HyderabadComponent } from './hyderabad/hyderabad.component';
+import { BangaloreComponent } from './bangalore/bangalore.component';
+import { ChennaiComponent } from './chennai/chennai.component';
+import { DelhiComponent } from './delhi/delhi.component';
+import { PuneComponent } from './pune/pune.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HyderabadComponent,
+    BangaloreComponent,
+    ChennaiComponent,
+    DelhiComponent,
+    PuneComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, BangaloreComponent, ChennaiComponent, DelhiComponent, HyderabadComponent, PuneComponent]
 })
 export class AppModule { }
